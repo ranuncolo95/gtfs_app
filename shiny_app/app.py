83ffd686ebf9ac1fb44301b940d8dea333727168
ranuncolo95 <alfonsomammato995@gmail.com>
2025-08-14 17:07:56 +0200

    first commit

---
+++ shiny_app/app.py
@@ -0,0 +1,31 @@
+from shiny import App, ui
+from maplibre import output_maplibregl, render_maplibregl, Map
+
+app_ui = ui.page_fluid(
+    ui.tags.style("""
+        html, body, .container-fluid, #map-container {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            width: 100vw;
+        }
+        #maplibre {
+            height: 100% !important;
+            width: 100% !important;
+        }
+    """),
+    ui.div(
+        output_maplibregl("maplibre"),
+        id="map-container",
+        class_="container-fluid"
+    )
+)
+
+def server(input, output, session):
+    @render_maplibregl
+    def maplibre():
+        m = Map(center=[9.12, 39.22], zoom=12)
+        return m
+    
+# Define the app
+app = App(app_ui, server)
